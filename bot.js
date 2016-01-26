772a5c85c81713e34da3e206ec0628f278222e97
Owen McNaughton <owen.mcnaughton@gmail.com>
2016-01-26 19:34:56 +0000

    working senses

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
-var botimg, bihw, bihh, bothigh, bhhw, bhhh, vision;
+var botimg, bihw, bihh, bothigh, bhhw, bhhh;
 var radius;
 var max_vel;
 var food_count, foodimg, fihw, fihh, food_particle, fooddie;
+var sense_miss = "rgba(200, 50, 50, .3)", sense_hit = "rgba(200, 200, 50, .5)";
 
 Init();
 
@@ -10,43 +11,41 @@ function Init() {
   botimg.src = 'res/bot.png';
   bihw = 4;
   bihh = 4;
-  
+
   bothigh = new Image();
   bothigh.src = 'res/bothigh.png';
   bhhw = 8;
   bhhh = 8;
-  
+
   radius = 8;
-  
-  max_vel = 4;
-  
+
+  max_vel = 1;
+
   food_count = 50;
-  
+
   foodimg = new Image();
   foodimg.src = 'res/food.png';
   fihw = 4;
   fihh = 4;
-  
+
   food_particle = new Image();
   food_particle.src = 'res/food_particle.png';
-  
+
   fooddie = new Image();
   fooddie.src = 'res/fooddie.png';
-  
-  vision = new Image();
-  vision.src = 'res/vision.png';
 }
 
 function Bot(world) {
   this.pos = new Victor(Random() * (world.width), Random() * (world.height));
   this.vel = new Victor(0, 0);
-  this.acc = new Victor(0.1, 0.1);
+  this.acc = new Victor(0, 0)
+                   .randomize(new Victor(-1, 1), new Victor(1, -1));
   this.rot = 0;
-  
+
   this.highlighted = false;
-  
+
   this.energy = 100;
-  
+
   this.beh = new Behaviour();
 }
 
@@ -54,9 +53,15 @@ Bot.prototype.Init = function(parent) {
   this.beh.Init(parent);
 };
 
+Bot.prototype.ValInit = function() {
+  this.vel = new Victor(0, 0);
+  this.acc = new Victor(0.1, 0.1);
+  this.beh.ValInit();
+};
+
 Bot.prototype.Update = function(world, dt) {
   this.acc = this.beh.Update(this, world);
-  
+
   this.vel.add(this.acc);
   if (this.vel.magnitude() > max_vel) {
     this.vel = this.vel.norm();
@@ -64,21 +69,22 @@ Bot.prototype.Update = function(world, dt) {
   }
   this.pos.add(this.vel);
   this.rot = this.vel.angle();
-  
+
   if (this.pos.x > world.width) this.pos.x = 0;
   if (this.pos.x < 0) this.pos.x = world.width - 1;
   if (this.pos.y > world.height) this.pos.y = 0;
   if (this.pos.y < 0) this.pos.y = world.height - 1;
-  
+
   for (var i = 0; i != world.food.length; i++) {
-    if (this.pos.distance(world.food[i]) < 7) {
-      world.FoodEaten(new Victor(this.pos.x, this.pos.y));
+    if (this.pos.distance(world.food[i].center) < 7) {
+      world.FoodEaten(world.food[i]);
       world.food.splice(i, 1);
       this.energy += 100;
       i--;
     }
   }
-  
+
+  return false;
   return this.energy < 0;
 };
 
@@ -100,9 +106,10 @@ Bot.prototype.Draw = function(ctx) {
     ctx.translate(this.pos.x, this.pos.y);
     ctx.rotate(this.rot);
     ctx.drawImage(botimg, -bihw, -bihh);
-    ctx = this.beh.Draw(ctx);
   }
   ctx.restore();
+  if (debug)
+    ctx = this.beh.Draw(ctx, this);
   return ctx;
 };
 
@@ -115,30 +122,79 @@ function Behaviour() {
     }
     this.routines.push(routine);
   }
-  
+
   this.r_ptr = 0;
   this.f_ptr = 0;
   this.repeat_cnt = 0;
-  
+
   this.senses = [];
-  this.senses.push({x: 0, y: 0, w: 80, h: 12});
-  this.senses.push({x: 0, y: 19, w: 70, h: 20});
-  this.senses.push({x: 0, y: -19, w: 70, h: 20});
+  this.senses.push({
+    p: new Victor(4, 0), q: new Victor(80, 0),
+    rp: new Victor(4, 0), rq: new Victor(80, 0), accuracy: 1, hit: false
+  });
+  this.senses.push({
+    p: new Victor(4, 0), q: new Victor(60, 30),
+    rp: new Victor(4, 0), rq: new Victor(60, 30), accuracy: 1, hit: false
+  });
+  this.senses.push({
+    p: new Victor(4, 0), q: new Victor(60, -30),
+    rp: new Victor(4, 0), rq: new Victor(60, -30), accuracy: 1, hit: false
+  });
 }
 
 Behaviour.prototype.Init = function(parent) {
-  
+
 };
 
-Behaviour.prototype.Draw = function(ctx) {
+Behaviour.prototype.ValInit = function() {
+  this.routines = [];
+  var routine = [];
+  for (var j = 0; j != 5; j++) {
+    routine.push({func: 2, repeats: 1});
+  }
+  this.routines.push(routine);
+
+  this.r_ptr = 0;
+  this.f_ptr = 0;
+  this.repeat_cnt = 0;
+
+  this.senses = [];
+  this.senses.push({
+    p: new Victor(4, 0), q: new Victor(80, 0),
+    rp: new Victor(4, 0), rq: new Victor(80, 0), accuracy: 1, hit: false
+  });
+  this.senses.push({
+    p: new Victor(4, 0), q: new Victor(60, 30),
+    rp: new Victor(4, 0), rq: new Victor(60, 30), accuracy: 1, hit: false
+  });
+  this.senses.push({
+    p: new Victor(4, 0), q: new Victor(60, -30),
+    rp: new Victor(4, 0), rq: new Victor(60, -30), accuracy: 1, hit: false
+  });
+}
+
+Behaviour.prototype.Draw = function(ctx, bot) {
   for (var i = 0; i != this.senses.length; i++) {
-    ctx.save();
     var r = this.senses[i];
-    ctx.translate(bihw, -r.h/2 + r.y);
-    ctx.drawImage(vision, 0, 0, 1, 1, 0, 0, r.w, r.h);
+    r.rp.x = r.p.x + bot.pos.x; 
+    r.rp.y = r.p.y + bot.pos.y;
+    r.rq.x = r.q.x + bot.pos.x; 
+    r.rq.y = r.q.y + bot.pos.y;
+    r.rp = RotatePointRad(r.rp, new Victor(r.rp.x-4, r.rp.y), bot.rot);
+    r.rq = RotatePointRad(r.rq, new Victor(r.rp.x-4, r.rp.y), bot.rot);
+    ctx.save();
+    if (r.hit) {
+      ctx.save();
+      ctx = DrawLineVic(ctx, r.rp, r.rq, sense_hit, 3);
+      ctx.restore();
+    } else {
+      ctx.save();
+      ctx = DrawLineVic(ctx, r.rp, r.rq, sense_miss, 3);
+      ctx.restore();
+    }
     ctx.restore();
   }
-  
+
   return ctx;
 };
 
@@ -161,6 +217,19 @@ Behaviour.prototype.Update = function(bot, world) {
     }
     this.repeat_cnt = 0;
   }
+
+  for (var i = 0; i != this.senses.length; i++) {
+    this.senses[i].hit = false;
+    for (var j = 0; j != world.food.length; j++) {
+      if (world.food[j].tl != undefined && world.food[j].tl != undefined) {
+        if (LineIntersects(this.senses[i].rp, this.senses[i].rq,
+                           world.food[j].tl, world.food[j].br)) {
+          this.senses[i].hit = true;
+        }
+      }
+    }
+  }
+
   return bot.acc;
 };
 
@@ -168,7 +237,7 @@ Behaviour.prototype.ToString = function() {
   var s = "";
   for (var i = 0; i != this.routines.length; i++) {
     for (var j = 0; j != this.routines[i].length; j++) {
-      s += "{" +this.routines[i][j].func + "," + this.routines[i][j].repeats;
+      s += "{" + this.routines[i][j].func + "," + this.routines[i][j].repeats;
       s += "}";
     }
     s += "\n";
@@ -182,7 +251,8 @@ Behaviour.prototype.Accelerate = function(vec) {
 };
 
 Behaviour.prototype.Decelerate = function(vec) {
-  vec.divide(1.1);
+  vec.x = vec.x/10;
+  vec.y = vec.y/10;
   return vec;
 };
 
@@ -196,17 +266,33 @@ Behaviour.prototype.TurnRight = function(vec) {
   return vec;
 };
 
-function World(w, h) {
+function World(w, h, m) {
   this.width = w;
   this.height = h;
-  
+  this.margin = m;
+
   this.food = [];
   for (var i = 0; i != food_count; i++) {
-    this.food.push(new Victor(Random2(this.width), Random2(this.height)));
+    this.NewFood();
   }
-  
+
   this.animations = [];
 }
+
+World.prototype.NewFood = function() {
+  var i = this.food.length;
+  this.food.push({
+    pos: undefined, tl: undefined, br: undefined, center: undefined
+  });
+  this.food[i].pos = new Victor(Random2(this.width-margin*2) + margin, 
+                                Random2(this.height-margin*2) + margin);
+  this.food[i].tl = new Victor(this.food[i].pos.x - 1, 
+                               this.food[i].pos.y - 1);
+  this.food[i].br = new Victor(this.food[i].pos.x + 9, 
+                               this.food[i].pos.y + 9);
+  this.food[i].center = new Victor(this.food[i].pos.x + 4, 
+                                   this.food[i].pos.y + 4);  
+};
 
 World.prototype.Update = function(dt) {
   for (var i = 0; i != this.animations.length; i++) {
@@ -216,45 +302,47 @@ World.prototype.Update = function(dt) {
     }
   }
   if (this.food.length < food_count) {
-    this.food.push(new Victor(Random2(this.width), Random2(this.height)));
+    this.NewFood();
   }
 };
 
 World.prototype.Draw = function(ctx) {
   for (var i = 0; i != this.food.length; i++) {
     ctx.save();
-    ctx.translate(this.food[i].x, this.food[i].y);
-    ctx.drawImage(foodimg, -fihw, -fihh);
+    ctx.translate(this.food[i].pos.x, this.food[i].pos.y);
+    ctx.drawImage(foodimg, 0, 0);
     ctx.restore();
+    
+    if (debug)
+      ctx = DrawLineVic(ctx, this.food[i].tl, this.food[i].br, "#FFFFFF", 1);
   }
-  
+
   for (var i = 0; i != this.animations.length; i++) {
     this.animations[i].Draw(ctx);
   }
   return ctx;
 };
 
-World.prototype.FoodEaten = function(pos) {
-  var velxs = [1, -1, -2, 2];
-  var velys = [1, -1, -2, 2];
-  for (var i = 0; i != 4; i++) {
-    for (var j = 0; j != 4; j++) {
-      var vel = new Victor(velxs[i], velys[j]);
-      this.animations.push(new Animated(pos, fooddie, 8, 16));
-    }
-  }
+World.prototype.FoodEaten = function(food) {
+  this.animations.push(new Animated(food.pos, fooddie, 8, 2, 16));
 };
 
-function Animated(pos, img, frame_count, size) {
+function Animated(pos, img, frame_count, fr, size) {
   this.pos = pos;
   this.frame = 0;
   this.img = img;
   this.frame_count = frame_count;
   this.size = size;
+  this.frame_repeat = fr;
+  this.frame_repeat_cnt = 0;
 }
 
 Animated.prototype.Update = function(dt) {
-  this.frame++;
+  this.frame_repeat_cnt++;
+  if (this.frame_repeat_cnt >= this.frame_repeat) {
+    this.frame++;
+    this.frame_repeat_cnt = 0;
+  }
   return this.frame == this.frame_count;
 };
 
@@ -263,7 +351,7 @@ Animated.prototype.Draw = function(ctx) {
   ctx.save();
   ctx.translate(this.pos.x, this.pos.y);
   ctx.drawImage(this.img, this.frame*this.size, 0, this.size, this.size,
-                0, 0, this.size, this.size);
+                -4, -4, this.size, this.size);
   ctx.restore();
   return ctx;
 };
--- util.js
+++ util.js
@@ -12,3 +12,54 @@ function Random2(upper) {
   var x = Math.sin(seed++) * 10000;
   return Math.round(((x - Math.floor(x))*upper));
 }
+
+function RotatePointIntl(px, py, ox, oy, angle) {
+  return {
+    x: Math.cos(angle) * (px-ox) - Math.sin(angle) * (py-oy) + ox,
+    y: Math.sin(angle) * (px-ox) + Math.cos(angle) * (py-oy) + oy
+  };
+}
+
+function RotatePointDeg(p, o, degs) {
+  var rads = degs * D2R;
+  return RotatePointIntl(p.x, p.y, o.x, o.y, rads);
+}
+
+function RotatePointRad(p, o, rads) {
+  return RotatePointIntl(p.x, p.y, o.x, o.y, rads);
+}
+
+function DrawLine(ctx, x1, y1, x2, y2, color, width) {
+  ctx.lineWidth = width;
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  return ctx;
+}
+
+function DrawLineVic(ctx, p, q, color, width) {
+  return DrawLine(ctx, p.x, p.y, q.x, q.y, color, width);
+  return ctx;
+}
+
+function LineIntersects(p0, p1, q0, q1) {
+  var s1_x, s1_y, s2_x, s2_y;
+  s1_x = p1.x - p0.x;
+  s1_y = p1.y - p0.y;
+  s2_x = q1.x - q0.x;
+  s2_y = q1.y - q0.y;
+
+  var s, t;
+  s = (-s1_y * (p0.x - q0.x) + s1_x * (p0.y - q0.y)) / 
+      (-s2_x * s1_y + s1_x * s2_y);
+  t = ( s2_x * (p0.y - q0.y) - s2_y * (p0.x - q0.x)) / 
+      (-s2_x * s1_y + s1_x * s2_y);
+
+  if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+    return true;
+  }
+
+  return false;
+}
